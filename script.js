fdb6aa68596ff136299dc31ad69c31eb19f37554
Patrick <Patrick@DESKTOP-PPQLOO7>
2020-12-31 05:43:10 +0100

    Commit

--- script.js
+++ script.js
@@ -1,76 +1,250 @@
 let i = 0;
-let local;
-let points = 1000;
+let localGameArray;
+let localScoreboardArray;
+let points;
 let score = 0;
+let interval;
+const scoresToDisplay = 10;
+const initPoints = 1000;
 
+let gameURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSscJOANgfVJlrdEFLhA8wo9SECKjas1J8l26_-ezt2n-c3MJqXtsTqwmk1tE_dQ0s1vQ4PpPpCwVb6/pub?output=csv';
+let scoreboardURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRjf9RbLjA6czrzQtmX8EEBXHHlnwWra78jWw-_9pUW2KtaOAwVk8PBkm0hpX1TOL8VfAu1jeldvHOz/pub?output=csv';
 
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/16XH56MWaPFG8bSGWp4UdhmR7og1OMNGIyvLf1FZUc1Q/edit?usp=sharing';
-
-function init() {
-    Tabletop.init({
-        key: publicSpreadsheetUrl,
-        callback: saveArray,
-        simpleSheet: true
-    })
+function initGame() {
+    Papa.parse(gameURL, {
+            download: true,
+          header: true,
+          complete: function(results) {
+            localGameArray = results.data
+            console.log(localGameArray)
+            startNewGame();
+          }
+        })
 };
 
-function saveArray(data, tabletop) {
-    local = data;
-    game();
+function initScoreboard() {
+    Papa.parse(scoreboardURL, {
+            download: true,
+          header: true,
+          complete: function(results) {
+            localScoreboardArray = results.data
+            console.log(localScoreboardArray)
+            document.getElementById("excelDataTable").innerHTML = ""
+            buildHtmlTable('#excelDataTable')
+          }
+        })
+};
+
+async function saveScoreboardArray(data, tabletop) {
+    localScoreboardArray = data;
+    buildHtmlTable('#excelDataTable')
+   // document.getElementById("scoreboard").innerHTML = (JSON.stringify(data.slice(0,10)));
+    return "DONE"
 }
 
-
-window.addEventListener('DOMContentLoaded', init);
+function buildHtmlTable(selector) {
+    var columns = addAllColumnHeaders(localScoreboardArray, selector);
+  
+    for (var i = 0; i < scoresToDisplay; i++) {
+      var row$ = $('<tr/>');
+      for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+        var cellValue = localScoreboardArray[i][columns[colIndex]];
+        if (cellValue == null) cellValue = "";
+        row$.append($('<td/>').html(cellValue));
+      }
+      $(selector).append(row$);
+    }
+  }
+  
+  // Adds a header row to the table and returns the set of columns.
+  // Need to do union of keys from all records as some records may not contain
+  // all records.
+  function addAllColumnHeaders(localScoreboardArray, selector) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+  
+    for (var i = 0; i < scoresToDisplay; i++) {
+      var rowHash = localScoreboardArray[i];
+      for (var key in rowHash) {
+        if ($.inArray(key, columnSet) == -1) {
+          columnSet.push(key);
+          headerTr$.append($('<th/>').html(key));
+        }
+      }
+    }
+    $(selector).append(headerTr$);
+  
+    return columnSet;
+  }
 
 window.onload = function () {
-    document.getElementById("clickMe").addEventListener("click", submitInput)
-    document.getElementById("joker").addEventListener("click", HideWrongHint)
-
+    document.getElementById("newGame").addEventListener("click", initGame);
+    document.getElementById("submitScore").addEventListener("click", showHighscores);
+    document.getElementById("showHighscore").addEventListener("click", showHighscores);
+    initScoreboard();
+    resetPoints();
 };
 
+function resetPoints() {
+    points = initPoints;
+}
+
+function startNewGame() {
+    document.getElementById("beforeGame").style.display = "none"
+    document.getElementById("runningGame").style.display = "block"
+    document.getElementById("clickMe").addEventListener("click", submitInput)
+    document.getElementById("joker").addEventListener("click", HideWrongHint)
+    game();
+}
 
 function HideWrongHint() {
     if (document.getElementById(local[i].FalscherHinweis).style.visibility != "hidden") {
         document.getElementById(local[i].FalscherHinweis).style.visibility = "hidden";
-        if (points >= 100) points -= 100;
+        if (points >= 100) points -= 100; else points = 0;
     }
 }
 
 function submitInput() {
-    if (document.getElementById('input').value == local[i].Antwort.toUpperCase()) {
+    clearInterval(interval);
+    if (document.getElementById('input').value == localGameArray[i].Antwort.toUpperCase()) {
         document.getElementById("input").value = "";
         console.log(points);
         score += points;
-        points = 1000;
+        resetPoints();
         document.getElementById("Score").innerHTML = "Score: " + score;
-        document.getElementById(local[i].FalscherHinweis).style.visibility = null;
+        document.getElementById(localGameArray[i].FalscherHinweis).style.visibility = null;
         i++;
         game();
     } else {
-    document.getElementById("runningGame").style.visibility = "hidden";
-    document.getElementById("gameOver").style.visibility = "visible";
+        document.getElementById("runningGame").style.display = "none"
+        document.getElementById("afterGame").style.display = "block";
     }
     ;
 }
 
+function showHighscores() {
+    document.getElementById("beforeGame").style.display = "block"
+    document.getElementById("runningGame").style.display = "none"
+    document.getElementById("afterGame").style.display = "none"
+    initScoreboard();
+    document.getElementById("scoreboard").style.display = "block"
+}
+
 function game() {
-    document.getElementById("QuestionNumber").innerHTML = "Frage Nr: " + local[i].FrageNr;
-    document.getElementById("lookingFor").innerHTML = "Gesucht ist: " + local[i].GesuchtIst;
-    document.getElementById("HintOne").innerHTML = local[i].HintOne;
-    document.getElementById("HintTwo").innerHTML = local[i].HintTwo;
-    document.getElementById("HintThree").innerHTML = local[i].HintThree;
-    document.getElementById("HintFour").innerHTML = local[i].HintFour;
-    document.getElementById("input").maxLength = local[i].Antwort.length;
-    document.getElementById("input").style.width = (1.5 * local[i].Antwort.length - 0.01) + 'ch';
-    let interval = setInterval(startTimer, 50);
+    document.getElementById("QuestionNumber").innerHTML = "Frage Nr: " + localGameArray[i].FrageNr;
+    document.getElementById("lookingFor").innerHTML = "Gesucht ist: " + localGameArray[i].GesuchtIst;
+    document.getElementById("HintOne").innerHTML = localGameArray[i].HintOne;
+    document.getElementById("HintTwo").innerHTML = localGameArray[i].HintTwo;
+    document.getElementById("HintThree").innerHTML = localGameArray[i].HintThree;
+    document.getElementById("HintFour").innerHTML = localGameArray[i].HintFour;
+    document.getElementById("input").maxLength = localGameArray[i].Antwort.length;
+    document.getElementById("input").style.width = (1.5 * localGameArray[i].Antwort.length - 0.01) + 'ch';
+    interval = setInterval(startTimer, 50);
 }
 
 function startTimer() {
-    console.log("TimerStarted");
     document.getElementById("points").innerHTML = "Punkte: " + points;
-    if (points < 1) {
-        clearInterval(interval);
-        return;
-    }
-    points -= 1;
+    if (points == 0) clearInterval(interval); else
+    points--;
 }
+
+(function() {
+    // get all data in form and return object
+    function getFormData(form) {
+      var elements = form.elements;
+      var honeypot;
+  console.log(Object.keys(elements))
+      var fields = Object.keys(elements).filter(function(k) {
+  
+        console.log(k);
+        console.log(elements[k].value);
+        if (elements[k].name === "honeypot") {
+          honeypot = elements[k].value;
+          return false;
+        }
+        return true;
+      }).map(function(k) {
+        if(elements[k].name !== undefined) {
+          return elements[k].name;
+        // special case for Edge's html collection
+        }else if(elements[k].length > 0){
+          return elements[k].item(0).name;
+        }
+      }).filter(function(item, pos, self) {
+        return self.indexOf(item) == pos && item;
+      });
+  
+      var formData = {};
+      fields.forEach(function(name){
+        var element = elements[name];
+        if (name === "Punkte")
+        formData[name] = score; else
+        formData[name] = element.value;
+        console.log("----------")
+        console.log(formData[name])
+        console.log("----------")
+      });
+  
+      // add form-specific values into the data
+      formData.formDataNameOrder = JSON.stringify(fields);
+      formData.formGoogleSheetName = form.dataset.sheet || "scores"; // default sheet name
+      formData.formGoogleSendEmail
+        = form.dataset.email || ""; // no email by default
+  
+      return {data: formData, honeypot: honeypot};
+    }
+  
+    function handleFormSubmit(event) {  // handles form submit without any jquery
+      event.preventDefault();           // we are submitting via xhr below
+      var form = event.target;
+      var formData = getFormData(form);
+      var data = formData.data;
+  
+      // If a honeypot field is filled, assume it was done so by a spam bot.
+      if (formData.honeypot) {
+        return false;
+      }
+  
+      disableAllButtons(form);
+      var url = form.action;
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      // xhr.withCredentials = true;
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            form.reset();
+            var formElements = form.querySelector(".form-elements")
+            if (formElements) {
+              formElements.style.display = "none"; // hide form
+            }
+            var thankYouMessage = form.querySelector(".thankyou_message");
+            if (thankYouMessage) {
+              thankYouMessage.style.display = "block";
+            }
+          }
+      };
+      // url encode form data for sending as post data
+      var encoded = Object.keys(data).map(function(k) {
+          return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+      }).join('&');
+      xhr.send(encoded);
+    }
+    
+    function loaded() {
+      // bind to the submit event of our form
+      var forms = document.querySelectorAll("form.gform");
+   //   console.log(forms.length)
+      for (var i = 0; i < forms.length; i++) {
+        forms[i].addEventListener("submit", handleFormSubmit, false);
+      }
+    };
+    document.addEventListener("DOMContentLoaded", loaded, false);
+  
+    function disableAllButtons(form) {
+      var buttons = form.querySelectorAll("button");
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
+    }
+  })();
